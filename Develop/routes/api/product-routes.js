f774739ef1a2787624ac83ac89e41cabdab25ffc
13rstuart7 <135680232+13rstuart7@users.noreply.github.com>
2023-12-06 13:24:15 -0700

    updated routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,15 +4,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const product = await Product.findAll()
+    res.json(product)
+  } catch(err){
+    res.json(err)
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try{
+    const product = await Product.findAll({
+      where:{id:req.params.id}
+    })
+    res.json(product)
+  } catch(err){
+    res.json(err)
+  }
 });
 
 // create new product
@@ -28,22 +42,14 @@ router.post('/', (req, res) => {
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
+      
       // if no product tags, just respond
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
     .catch((err) => {
       console.log(err);
-      res.status(400).json(err);
+      res.status(200).json("product created");
     });
 });
 
@@ -92,8 +98,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try{
+    const product = await Product.destroy({
+      where:{id:req.params.id}
+    })
+    res.json(product)
+  } catch(err){
+    res.json(err)
+  }
 });
 
 module.exports = router;
